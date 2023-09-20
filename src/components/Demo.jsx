9ef2ad95318c0981685ff5b28540e18f4e1e82e5
Hashwi <119130028+Hashwi@users.noreply.github.com>
2023-09-20 15:38:35 +0200

    Adding redux to mage states

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -2,12 +2,20 @@ import React, { useState, useEffect } from "react";
 import { copy, linkIcon, loader, tick } from "../assets";
 
 const Demo = () => {
+  const [article, setArticle] = useState({
+    url: '',
+    summary: '',
+  })
+
+  const handleSubmit = (e)=> {
+    alert('Submitted')
+  }
   return (
     <section className="mt-16 w-full max-w-xl">
       <div className="flex flex-col w-full gap-2">
-        <form
+        <form 
           className="relative flex justify-center items-center"
-          onSubmit={() => {}}>
+          onSubmit={handleSubmit}>
           <img
             src={linkIcon}
             alt="link_icon"
@@ -16,15 +24,18 @@ const Demo = () => {
           <input
             type="url"
             placeholder="Enter a URL"
-            value=""
-            onChange={() => {}}
+            value={article.url}
+            onChange={(e) => setArticle({ ...article, url: e.target.value})}
             required
             className="url_input peer"
           />
-          <button type="submit" className="submit_btn">
-          🚀
+          <button
+            type="submit"
+            className="submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700  ">
+            🚀
           </button>
         </form>
+
       </div>
     </section>
   );
